c7397f9290a23fd2cdcf1132ba09e388283dbd33
sahil <sahiillx02@gmail.com>
2024-12-11 16:46:07 +0530

    update prev

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ const Calculator = () => {
       <button className="copy" onClick={handleCopy} style={{ position: 'absolute', left: '10px', bottom: '7px' }}>
         <span data-text-end="Copied!" data-text-initial="Copy to clipboard" className="tooltip" />
         <span>
-          <svg xmlns="http://www.w3.org/2000/svg" width="1.2rem" height="1.2rem" viewBox="0 0 24 24">
+          <svg xmlns="http://www.w3.org/2000/svg" width="1rem" height="1rem" viewBox="0 0 24 24">
             <g fill="none">
               <path fill="#ffffff" fill-opacity="0.16" d="M18.6 9h-7.2A2.4 2.4 0 0 0 9 11.4v7.2a2.4 2.4 0 0 0 2.4 2.4h7.2a2.4 2.4 0 0 0 2.4-2.4v-7.2A2.4 2.4 0 0 0 18.6 9" />
               <path stroke="#ffffff" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="1.5" d="M6 15h-.6C4.07 15 3 13.93 3 12.6V5.4C3 4.07 4.07 3 5.4 3h7.2C13.93 3 15 4.07 15 5.4V6m-3.6 3h7.2a2.4 2.4 0 0 1 2.4 2.4v7.2a2.4 2.4 0 0 1-2.4 2.4h-7.2A2.4 2.4 0 0 1 9 18.6v-7.2A2.4 2.4 0 0 1 11.4 9" />
